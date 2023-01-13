e67a869056a3f8895e1343682b23c0a239d13825
zhoeks <zhoeks@yahoo.com>
2023-01-13 13:22:22 -0600

    Final solution adding return and passing tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,5 @@
-const leapYears = function() {
-    arguments % 400 === 0 ? true : arguments % 100 === 0 ? false : arguments % 4 === 0 ? true : false;
+const leapYears = function(number) {
+  return number % 400 === 0 ? true : number % 100 === 0 ? false : number % 4 === 0 ? true : false;
 };
 
 // Do not edit below this line
