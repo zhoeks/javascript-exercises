60a9ff4cb44dc549d26c0c64afdb45db12ea0f35
zhoeks <zhoeks@yahoo.com>
2023-01-13 06:59:42 -0600

    solutions to first 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = '';
+    if (num >= 0) {
+    for (let i = 0; i < num; i++) {
+        newString += string;
+    }
+    return newString;
+} else {
+    return 'ERROR'
+}
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
