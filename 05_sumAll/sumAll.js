4ae9cc7f2324a590c716a841ea23cfb1d359f102
zhoeks <zhoeks@yahoo.com>
2023-01-13 13:08:49 -0600

    Added notes for clarity

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,12 @@
 const sumAll = function() {
+    //Confirm all elements are positive integers
     let numberCheck = [...arguments].every(function (test) {
         return test >= 0 && typeof test == 'number';
     })
+    //sort array min to max
     let sortedArray = [Math.min(...arguments), Math.max(...arguments)];
     let returnValue = 0;
-
+    //iterate through sorted array adding each iteration to the total
     if (numberCheck) {
     for (let i = sortedArray[0]; i <= sortedArray[1]; i++) {
         returnValue += i;
