c3b47fc87d416d11cb028cb20e1332a8a08bfdcb
zhoeks <zhoeks@yahoo.com>
2023-01-13 12:58:46 -0600

    Added every method to determine entry type

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,18 @@
-const sumAll = function(...arguments) {
+const sumAll = function() {
+    let numberCheck = [...arguments].every(function (test) {
+        return test >= 0
+    })
     let sortedArray = [Math.min(...arguments), Math.max(...arguments)];
     let returnValue = 0;
+
+    if (numberCheck) {
     for (let i = sortedArray[0]; i <= sortedArray[1]; i++) {
         returnValue += i;
-    }
-    
+    } 
+} else {
+    return "ERROR";
+}
+      
     return returnValue;
 };
 
