eb7612c2cb7db7981290e5543c2954bdd3d5682d
zhoeks <zhoeks@yahoo.com>
2023-01-13 11:19:05 -0600

    Initial code to return array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,12 +2,9 @@ const removeFromArray = function() {
     //split arguments into array to return and arguments to remove
     let array = arguments[0];
     let args = [...arguments].splice(1);
-    let indexToRemove = [];
-    for (let i = 0; i < args.length; i++) {
-        indexToRemove.push(array.indexOf(args[i]))
-    }
-    
-    console.log(indexToRemove);
+    let returnArray = array.filter( (element) => !args.includes(element) );
+
+    console.log(returnArray);
 };
 
 // Do not edit below this line
