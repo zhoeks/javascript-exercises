3af3dcd77bf5d9ff25ac7059c1dcb10c991bbd72
zhoeks <zhoeks@yahoo.com>
2023-01-13 10:50:27 -0600

    created iteration to retrieve index location of items to remove

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,13 @@
 const removeFromArray = function() {
+    //split arguments into array to return and arguments to remove
     let array = arguments[0];
     let args = [...arguments].splice(1);
-    console.log(args)
+    let indexToRemove = [];
+    for (let i = 0; i < args.length; i++) {
+        indexToRemove.push(array.indexOf(args[i]))
+    }
+    
+    console.log(indexToRemove);
 };
 
 // Do not edit below this line
